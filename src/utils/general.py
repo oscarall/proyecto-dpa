2695a7c5fe364762360811f3998addc49b440c82
platapia <77368881+platapia@users.noreply.github.com>
2021-05-16 15:24:50 -0500

    Update general.py

--- src/utils/general.py
+++ src/utils/general.py
@@ -21,3 +21,8 @@ def get_db_credentials(credentials_file) -> dict:
 def get_api_token(credentials_file) -> str:
     token = read_yaml(credentials_file)["food_inspections"]["api_token"]
     return token
+
+def get_db_conn_sql_alchemy(credentials_file) -> dict:
+    alchemy_credentials = read_yaml(credentials_file)["db"]
+    connection = "postgresql://{}:{}@{}:{}/{}".format(alchemy_credentials['user'], alchemy_credentials['pass'], alchemy_credentials['host'], alchemy_credentials['port'], alchemy_credentials['db']) 
+    return connection
